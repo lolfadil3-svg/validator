f73502ffe04ca10327775c50ad3cd6058ef4cab2
lolfadil3-svg <lolfadil3@gmail.com>
2026-03-03 04:34:00 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import checkCache from './handler'
+return new Response(JSON.stringify(data), {
+  headers: {
+    "content-type": "application/json",
+    "Access-Control-Allow-Origin": "*", // INI YANG WAJIB ADA
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  },
+});
 
-export default {
-  fetch: async (request: Request): Promise<Response> => await checkCache(request)
-}
+Setelah Anda mengedit file tersebut di GitHub, tunggu sampai GitHub Actions berubah menjadi centang hijau lagi, baru kemudian coba websitenya. Selamat mencoba!
